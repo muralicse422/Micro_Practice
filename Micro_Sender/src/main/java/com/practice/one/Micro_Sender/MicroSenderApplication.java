2edc86706e50729fbde320f2d3c75c19b1804948
muralicse422 <111060152+muralicse422@users.noreply.github.com>
2024-07-25 22:25:06 +0530

    Add files via upload
    First commit for project

---
+++ Micro_Sender/src/main/java/com/practice/one/Micro_Sender/MicroSenderApplication.java
@@ -0,0 +1,13 @@
+package com.practice.one.Micro_Sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSenderApplication.class, args);
+	}
+
+}
---
+++ Micro_Sender/src/test/java/com/practice/one/Micro_Sender/MicroSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.one.Micro_Sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
