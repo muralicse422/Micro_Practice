2773a76794c1bf0feb973e85aab2857b8b810ac6
muralicse422 <111060152+muralicse422@users.noreply.github.com>
2024-07-26 00:07:10 +0530

    Add files via upload
    Added Repository Micro_Practice_Server

---
+++ Micro_Practice_Server/src/main/java/com/Micro_Practice_Server/one/Micro_Practice_Server/MicroPracticeServerApplication.java
@@ -0,0 +1,13 @@
+package com.Micro_Practice_Server.one.Micro_Practice_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPracticeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPracticeServerApplication.class, args);
+	}
+
+}
---
+++ Micro_Practice_Server/src/test/java/com/Micro_Practice_Server/one/Micro_Practice_Server/MicroPracticeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Micro_Practice_Server.one.Micro_Practice_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPracticeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
