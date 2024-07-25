ea808a7355b9c49839b43e1f70633a0fbad3d606
unknown <muralicse42@gmail.com>
2024-07-26 00:51:19 +0530

    added disovery service properites.

--- Micro_Practice_Server/src/main/java/com/Micro_Practice_Server/one/Micro_Practice_Server/MicroPracticeServerApplication.java
+++ Micro_Practice_Server/src/main/java/com/Micro_Practice_Server/one/Micro_Practice_Server/MicroPracticeServerApplication.java
@@ -2,8 +2,10 @@ package com.Micro_Practice_Server.one.Micro_Practice_Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroPracticeServerApplication {
 
 	public static void main(String[] args) {
