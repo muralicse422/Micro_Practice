29cafe0e623560188899f8c70b6ed79d03ffa554
muralicse422 <muralicse42@gmail.com>
2024-07-30 23:31:35 +0530

    Added Sequence code

--- Micro_Sender/src/main/java/com/practice/one/Micro_Sender/model/Sender.java
+++ Micro_Sender/src/main/java/com/practice/one/Micro_Sender/model/Sender.java
@@ -1,2 +1,43 @@
-package com.practice.one.Micro_Sender.model;public class Sender {
+package com.practice.one.Micro_Sender.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name ="sender_info")
+public class Sender {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "t_generator")
+    @SequenceGenerator(name = "t_generator", allocationSize = 50, sequenceName = "TABLE_SEQUENCE", initialValue=1)
+    private long id;
+    private String message;
+    @Column(name ="sender_date")
+    private String date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
