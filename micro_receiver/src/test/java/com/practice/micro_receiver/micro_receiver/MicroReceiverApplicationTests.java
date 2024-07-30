f97cce4e6afd8c126f0c7bc44b2ed6377276897b
muralicse422 <111060152+muralicse422@users.noreply.github.com>
2024-07-30 23:51:00 +0530

    Add files via upload
    Commiting intial code base of micro reciever

---
+++ micro_receiver/src/test/java/com/practice/micro_receiver/micro_receiver/MicroReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.micro_receiver.micro_receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
